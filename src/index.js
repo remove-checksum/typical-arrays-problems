1c909c308d406882c22ae5fd955ad36f10fe909a
German <bz5h7@yandex.ru>
2021-02-28 23:09:00 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (arguments.length && array.length) ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (arguments.length && array.length) ? array.sort((a, b) => b - a)[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (arguments.length && array.length) ? (array.reduce((acc, num) => acc + num) / array.length) : 0;
 }
